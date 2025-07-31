c9bff4444b541df5480a3bc6e90105ea94695e02
Fundi1330 <kordaaroma@gmail.com>
2025-07-31 20:51:48 +0300

    misc: changed flask_config.py

--- backend/app/flask_config.py
+++ backend/app/flask_config.py
@@ -3,5 +3,13 @@ import os
 # A config file for flask only
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}'
+user = os.environ.get("DB_USER", "")
+password = os.environ.get("DB_PASSWORD", "")
+host = os.environ.get("DB_HOST", "")
+port = os.environ.get("DB_PORT", "")
+dbname = os.environ.get("DB_NAME", "")
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://{user}:{password}"
+    f"@{host}:{port}/{dbname}"
+)
 CORS_URL = os.environ.get('CORS_URL')
