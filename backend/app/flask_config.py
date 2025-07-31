f4fc5f700bd371ce6341443dd691c7e55e015d5c
Fundi1330 <kordaaroma@gmail.com>
2025-07-31 19:52:19 +0300

    feat: change db to postgres

--- backend/app/flask_config.py
+++ backend/app/flask_config.py
@@ -3,5 +3,5 @@ import os
 # A config file for flask only
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-CORS_URL = 'http://localhost:5173'
+SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}'
+CORS_URL = 'https://reading-list-6nye.onrender.com/'
